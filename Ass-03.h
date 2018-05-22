3d636c70cca49cd299783262c11dabe86023a960
Italiant <39428153+Italiant@users.noreply.github.com>
2018-05-22 13:11:42 +1000

    Updated to v3 changes

--- Ass-03.h
+++ Ass-03.h
@@ -1,5 +1,5 @@
-//     $Date: 2018-05-10 10:07:15 +1000 (Thu, 10 May 2018) $
-// $Revision: 1301 $
+//     $Date: 2018-05-22 06:24:02 +1000 (Tue, 22 May 2018) $
+// $Revision: 1330 $
 //   $Author: Peter $
 
 // Assignment 3 include file
@@ -17,6 +17,7 @@
 #include "touch_panel.h"
 #include "openx07v_c_lcd.h"
 #include "fatfs.h"
+#include "adc.h"
 
 #include <stdio.h>
 #include <stdint.h>
@@ -29,10 +30,15 @@
 extern osThreadId defaultTaskHandle;
 extern osThreadId myTask02Handle;
 extern osThreadId myTask03Handle;
+extern osThreadId myTask04Handle;
 extern osTimerId myTimer01Handle;
 
 extern osSemaphoreId myBinarySem01Handle;
 extern osSemaphoreId myBinarySem02Handle;
+extern osSemaphoreId myBinarySem03Handle;
+extern osSemaphoreId myBinarySem04Handle;
+extern osSemaphoreId myBinarySem05Handle;
+extern osSemaphoreId myBinarySem06Handle;
 extern osMessageQId myQueue01Handle;
 extern osMutexId myMutex01Handle; // Protect LCD
 extern osMutexId myMutex02Handle; // Protect console output
@@ -41,10 +47,14 @@ extern osMutexId myMutex02Handle; // Protect console output
 extern void Ass_03_Task_01(void const *argument);
 extern void Ass_03_Task_02(void const *argument);
 extern void Ass_03_Task_03(void const *argument);
+extern void Ass_03_Task_04(void const *argument);
 
 // Library functions
 extern uint8_t BSP_TP_Init(void);
 extern uint8_t BSP_TP_GetDisplayPoint(Coordinate *pDisplay);
+
+// Front panel input
+extern uint8_t getfp(Coordinate *display);
 
 // STEPIEN: Safe printf() to ensure that only one task can write to
 //          the console at a time
