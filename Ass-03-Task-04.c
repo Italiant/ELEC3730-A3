9e9f2547b83cbecbf55e1f9f9549a24042f60890
Italiant <39428153+Italiant@users.noreply.github.com>
2018-06-01 15:02:08 +1000

    Update Ass-03-Task-04.c

--- Ass-03-Task-04.c
+++ Ass-03-Task-04.c
@@ -18,101 +18,108 @@ uint16_t ADC_Value[1000];
 
 void Ass_03_Task_04(void const * argument)
 {
-  uint16_t i;
-  HAL_StatusTypeDef status;
-  uint16_t xpos=0;
-  uint16_t ypos=0;
-  uint16_t last_xpos=0;
-  uint16_t last_ypos=0;
+	uint16_t i;
+	HAL_StatusTypeDef status;
+	uint16_t xpos=0;
+	uint16_t ypos=0;
+	uint16_t last_xpos=0;
+	uint16_t last_ypos=0;
 
- // osEvent event;
-  uint32_t start = 1;
+	// osEvent event;
+	uint32_t start = 1;
+	uint32_t analog;
 
 #define XOFF 134
 #define YOFF 5
 #define XSIZE 182
 #define YSIZE 188
 
-  osSignalWait(1,osWaitForever);
-  safe_printf("Hello from Task 4 - Analog Input (turn ADC knob or use pulse sensor)\n");
+	osSignalWait(1,osWaitForever);
+	safe_printf("Hello from Task 4 - Analog Input (turn ADC knob or use pulse sensor)\n");
 
-  // Draw a box to plot in
- /* osMutexWait(myMutex01Handle, osWaitForever);
+	// Draw a box to plot in
+	/* osMutexWait(myMutex01Handle, osWaitForever);
   BSP_LCD_DrawRect(XOFF-1,YOFF-1,XSIZE+1,YSIZE+1);
   osMutexRelease(myMutex01Handle);*/
 
-  // Start the conversion process
-  status = HAL_ADC_Start_DMA(&hadc1, (uint32_t *)&ADC_Value, 1000);
-  if (status != HAL_OK)
-  {
-	  safe_printf("ERROR: Task 4 HAL_ADC_Start_DMA() %d\n", status);
-  }
+	// Start the conversion process
+	status = HAL_ADC_Start_DMA(&hadc1, (uint32_t *)&ADC_Value, 1000);
+	if (status != HAL_OK)
+	{
+		safe_printf("ERROR: Task 4 HAL_ADC_Start_DMA() %d\n", status);
+	}
 
-  // Start main loop
-  while (1)
-  {
+	osEvent event1, event2;
 
-	  osEvent event1;
+	// Start main loop
+	while (1)
+	{
+		event1 = osMessageGet(myQueue02Handle, 5);
+		if (event1.status == osEventMessage)
+		{
+			start = event1.value.v;
+		}
 
-	  	event1 = osMessageGet(myQueue02Handle, 5);
-	      if (event1.status == osEventMessage)
-	      {
-	    	  start = event1.value.v;
-	      }
+		event2 = osMessageGet(myQueue03Handle, 5);
+		if (event2.status == osEventMessage)
+		{
+			analog = event2.value.v;
+			safe_printf("Plotting time changed to (%d)s\n", analog);
+		}
 
-	  if(start == 1){
+		if(start == 1){
 
-	  // Wait for first half of buffer
-	  osSemaphoreWait(myBinarySem05Handle, osWaitForever);
-	  osMutexWait(myMutex01Handle, osWaitForever);
-	  for(i=0;i<500;i=i+500)
-	  {
-		  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-		  BSP_LCD_DrawVLine(XOFF+xpos,YOFF,YSIZE);
-		  BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-		  ypos=(uint16_t)((uint32_t)(ADC_Value[i])*YSIZE/4096);
-		  BSP_LCD_DrawLine(XOFF+last_xpos,YOFF+last_ypos,XOFF+xpos,YOFF+ypos);
-		  // BSP_LCD_FillRect(xpos,ypos,1,1);
-		  last_xpos=xpos;
-		  last_ypos=ypos;
-		  xpos++;
-	  }
-	  osMutexRelease(myMutex01Handle);
-	  if (last_xpos>=XSIZE-1)
-	  {
-		  xpos=0;
-		  last_xpos=0;
-	  }
+			// Wait for first half of buffer
+			osSemaphoreWait(myBinarySem05Handle, osWaitForever);
+			osMutexWait(myMutex01Handle, osWaitForever);
+			for(i=0;i<500;i=i+500)
+			{
+				BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+				BSP_LCD_DrawVLine(XOFF+xpos,YOFF,YSIZE);
+				BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+				ypos=(uint16_t)((uint32_t)(ADC_Value[i])*YSIZE/4096);
+				BSP_LCD_DrawLine(XOFF+last_xpos,YOFF+last_ypos,XOFF+xpos,YOFF+ypos);
+				// BSP_LCD_FillRect(xpos,ypos,1,1);
+				last_xpos=xpos;
+				last_ypos=ypos;
+				xpos++;
+			}
+			osMutexRelease(myMutex01Handle);
+			if (last_xpos>=XSIZE-1)
+			{
+				xpos=0;
+				last_xpos=0;
+			}
 
-	  // Wait for second half of buffer
-	  osSemaphoreWait(myBinarySem06Handle, osWaitForever);
-	  HAL_GPIO_WritePin(GPIOD, LD4_Pin, GPIO_PIN_SET);
-	  osMutexWait(myMutex01Handle, osWaitForever);
-	  for(i=0;i<500;i=i+500)
-	  {
-		  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-		  BSP_LCD_DrawVLine(XOFF+xpos,YOFF,YSIZE);
-		  BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-		  ypos=(uint16_t)((uint32_t)(ADC_Value[i+500])*YSIZE/4096);
-		  BSP_LCD_DrawLine(XOFF+last_xpos,YOFF+last_ypos,XOFF+xpos,YOFF+ypos);
-		  // BSP_LCD_FillCircle(xpos,ypos,2);
-		  last_xpos=xpos;
-		  last_ypos=ypos;
-		  xpos++;
-	  }
-	  osMutexRelease(myMutex01Handle);
-	  if (last_xpos>=XSIZE-1)
-	  {
-		  xpos=0;
-		  last_xpos=0;
-	  }
-	  HAL_GPIO_WritePin(GPIOD, LD4_Pin, GPIO_PIN_RESET);
+			// Wait for second half of buffer
+			osSemaphoreWait(myBinarySem06Handle, osWaitForever);
+			HAL_GPIO_WritePin(GPIOD, LD4_Pin, GPIO_PIN_SET);
+			osMutexWait(myMutex01Handle, osWaitForever);
+			for(i=0;i<500;i=i+500)
+			{
+				BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+				BSP_LCD_DrawVLine(XOFF+xpos,YOFF,YSIZE);
+				BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+				ypos=(uint16_t)((uint32_t)(ADC_Value[i+500])*YSIZE/4096);
+				BSP_LCD_DrawLine(XOFF+last_xpos,YOFF+last_ypos,XOFF+xpos,YOFF+ypos);
+				// BSP_LCD_FillCircle(xpos,ypos,2);
+				last_xpos=xpos;
+				last_ypos=ypos;
+				xpos++;
+			}
+			osMutexRelease(myMutex01Handle);
+			if (last_xpos>=XSIZE-1)
+			{
+				xpos=0;
+				last_xpos=0;
+			}
+			HAL_GPIO_WritePin(GPIOD, LD4_Pin, GPIO_PIN_RESET);
 
-	  }
-	  else{
+		}
+		else{
 
-	  }
-  }
+		}
+	}
 }
 
 // STEPIEN: Add callback functions to see if this can be used for double buffering equivalent
